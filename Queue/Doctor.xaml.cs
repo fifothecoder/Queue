3e3002b3fe699927f93a1b4560007b7314d5bd15
Andrej Kapusta <andrej.kapusta@studentstc.sk>
2019-03-30 13:57:54 +0100

    Navigate

--- Queue/Doctor.xaml.cs
+++ Queue/Doctor.xaml.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -14,35 +12,19 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.UI.Popups;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Queue
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Doctor : Page
     {
-        public MainPage()
+        public Doctor()
         {
             this.InitializeComponent();
-        }
-
-        private void Doctor_SignIn_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(Doctor));
-        }
-
-        private void Registraton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Patient_SignIn_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
