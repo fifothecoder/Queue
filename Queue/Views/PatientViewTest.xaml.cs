85bd835f5e372fb8cd33a18ade8e4e9ac928c3db
Andrej Kapusta <andrej.kapusta@studentstc.sk>
2019-03-31 00:51:38 +0100

    Final UI

--- Queue/Views/PatientViewTest.xaml.cs
+++ Queue/Views/PatientViewTest.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -35,5 +36,17 @@ namespace Queue
 
             _birthNum = e.Parameter.ToString();
         }
+
+        private void MainMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void LogOffButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(LoginPatientView));
+        }
+        
+
     }
 }
