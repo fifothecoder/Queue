13d69cb5bbee7440ef0b5af497ab59277b292670
Filip Biľ <filip112b@gmail.com>
2019-03-30 12:14:57 +0100

    First Commit

--- Queue/MainPage.xaml.cs
+++ Queue/MainPage.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +28,7 @@ namespace Queue
         public MainPage()
         {
             this.InitializeComponent();
+            //Sem pojde kod
         }
     }
 }
