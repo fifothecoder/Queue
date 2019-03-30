621622f1c57fcdf189f97988af7b1e8e7b52339b
Filip Biľ <filip112b@gmail.com>
2019-03-30 13:13:15 +0100

    Patient class created

---
+++ Queue/Classes/Patient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue.Classes
+{
+
+    public enum InsuranceComp
+    {
+        None = 0,
+        Dovera = 1,
+        VSZP = 2,
+        Union = 3
+    }
+
+    public class Patient
+    {
+
+        private readonly string _name;
+        private readonly string _sur;
+        private readonly InsuranceComp _ins;
+
+        public Patient(string nam, string sur, InsuranceComp ins)
+        {
+            _name = nam;
+            _sur = sur;
+            _ins = ins;
+        }
+
+        public string GetPatientName()
+        {
+            return $"{_name} {_sur}";
+        }
+
+        public string GetInsuranceCompany()
+        {
+            return _ins.ToString();
+        }
+
+        private bool ValidateName()
+        {
+            return true;
+        }
+
+        private bool ValidateSurname()
+        {
+            return true;
+        }
+
+    }
+}
